6596242ab2ed848483171dd33600dd97316a4310
Leosdc <leonardo.dc.work@gmail.com>
2026-03-26 16:32:14 -0300

    feat: Correção para deploy via Github Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Guia-do-Cidadao/',
   plugins: [react()],
   server: {
     proxy: {
